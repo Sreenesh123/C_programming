e543cc2f58bcc6f7b0be6447be194ed42dbaaaea
Sreenesh123 <143419702+Sreenesh123@users.noreply.github.com>
2023-10-28 20:19:46 +0530

    Update multiply_floating_numbers.cpp

--- Lab 2/Lab 2/multiply_floating_numbers.cpp
+++ Lab 2/Lab 2/multiply_floating_numbers.cpp
@@ -3,8 +3,10 @@
 int main()
 {
 	float a,b,c;
+	printf("Enter the value of two numbers: ");
 	scanf("%f %f",&a,&b);
 	c=a*b;
-	printf("The product is :  %f",c);
+	printf("The product in float form is  :  %f",c);
+	printf("The product in int form is  :  %d",c);
 	return 0;
 }
