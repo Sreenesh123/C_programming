8339cff184d2a24a72e8e340311483358ea36835
Sreenesh123 <143419702+Sreenesh123@users.noreply.github.com>
2023-10-28 22:41:35 +0530

    Update electricity bill.cpp

--- Lab 2/Lab 2/electricity bill.cpp
+++ Lab 2/Lab 2/electricity bill.cpp
@@ -5,9 +5,9 @@ int main()
 	printf("Enter the number of units: ");
 	scanf("%d",&u);
 	if (u>=0 && u<=100){
-	printf("The bill is %d",u*2);}
+	printf("The bill is %d rupees",u*2);}
 	else if (u>=101 && u<=250){
-	printf("The bill is %f",u*3.5);}
+	printf("The bill is %f rupees",u*3.5);}
 	else{
 	printf("The bill is %d rupees",u*5);}
 	
