ee410cc067d94259c83a164e96a109e35957c0ac
Sreenesh123 <143419702+Sreenesh123@users.noreply.github.com>
2023-10-28 20:01:45 +0530

    Update Area_of_circle.cpp

--- Lab 1 04.09.2023/Area_of_circle.cpp
+++ Lab 1 04.09.2023/Area_of_circle.cpp
@@ -1,11 +1,10 @@
 #include<stdio.h>
-int b=3.14;
 int main()
 {
 	float r,a;
 	printf("Enter the radius of the circle : " );
 	scanf("%f",&r);
-	int b=3.14;
+	float b=3.14;
 	a=b*r*r;
 	printf("The area is %f",a);
 	return 0;
