5d2d2c4db0554f4dbcb12874d425ed4db7242132
Sreenesh123 <143419702+Sreenesh123@users.noreply.github.com>
2023-11-05 17:24:03 +0530

    Update frequency_of_element.cpp

--- lab 4/frequency_of_element.cpp
+++ lab 4/frequency_of_element.cpp
@@ -1,29 +1,36 @@
 #include<stdio.h>
 int main(){
-	int n,i,t,c,k,j;
-	printf("Enter the number of elements in the array  : ");
+	int i,j,k,n,c,x,t;
+	printf("Enter the even number of elements in the array : ");
 	scanf("%d",&n);
 	int a[n];
+	printf("Enter the value of k : ");
+	scanf("%d",&k);
+	printf("Enter the elements in the array : ");
 	for (i=0;i<n;i++){
-		printf("a[%d] = ",i);
+		printf("\na[%d] : ",i);
 		scanf("%d",&a[i]);
-	}
-	printf("The array is : ");
-	printf("[");
-	for (i=0;i<n;i++){
-		printf("%d ",a[i]);}
-	printf("]/n");
-	
+		}
+	c=0;
 	for (i=0;i<n;i++){
 		c=0;
 		for (j=0;j<n;j++){
-			if (a[i]==a[j] ){
-				c+=1;
+			if (a[i]==a[j]){
+				c++;
+			}}
+			if(a[i]==k){
+				printf("the number of times %d occurs is : %d",k,c);
+			break;
 			}
-		
+			else {
+				if(i==n-1){
+					printf("The number is not available");
+				}
+			}
+			
 		}
-		printf("\nFrequency of %d is %d",a[i],c);
-
-		
 	
-	}return 0;}
+
+
+	return 0;
+}
